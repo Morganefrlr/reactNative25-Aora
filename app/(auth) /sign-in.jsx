b1fbec0038f5220422653a2e1867dce053d2f1dc
Morganefrlr <106095574+Morganefrlr@users.noreply.github.com>
2025-01-22 11:09:58 +0100

    signInPage

--- app/(auth) /sign-in.jsx
+++ app/(auth) /sign-in.jsx
@@ -1,12 +1,44 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, ScrollView, Image } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import {images} from '../../constants'
+
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView style={styles.safeAreaView}>
+      <ScrollView>
+        <View style={styles.viewStyle}>
+          <Image source={images.logo} resizeMode='contain' style={styles.logo}/>
+        <Text style={styles.textLog}>Log in to Aora</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
 export default SignIn
+
+const styles = StyleSheet.create({
+  safeAreaView:{
+    height:'100%',
+    backgroundColor:'#161622'
+  },
+  viewStyle:{
+    width:'100%',
+    height:'100%',
+    justifyContent:'center',
+    paddingLeft:16,
+    marginTop:24,
+  },
+  logo:{
+    width: 115,
+    height:35,
+  },
+  textLog:{
+    fontSize: 24,
+    color:'#fff',
+    fontWeight:'600',
+    marginTop: 40,
+    fontFamily:"Poppins-SemiBold",
+  }
+})
